ea4c304ab8a6ba7c9a60cbc80c51c20decbf4587
Your Namethefullm0nty <you@example.commontgomery.evan@gmail.com>
2020-01-15 21:20:35 +0000

    Formatted details page correctly with hard coded info

--- client/components/ProductDetails.jsx
+++ client/components/ProductDetails.jsx
@@ -9,6 +9,7 @@ export default class ProductDetails extends React.Component {
   }
 
   componentDidMount(productId) {
+    // console.log("props in app fetch: ", props)
     const init = {
       method: 'GET'
     };
@@ -20,19 +21,42 @@ export default class ProductDetails extends React.Component {
   render() {
     return (
       <div className={'container w-75 p-3 bg-secondary'}>
-        <div className={'backToCatalog'} onClick={() => this.props.setView('catalog', {})}>Back to Catalog</div>
-        <img className={'productDetailImage'} src='./images/shamwow.jpg' style={{ height: '200px', width: '200px' }}></img>
-        <div className={'productDetailsContainer'}>
-          <h5 className={'productDetailsTitle'}>SHAMWOWZERS</h5>
-          <h6 className={'productDetailPrice'}>$1000</h6>
-          <p className={'productDetailShortDescription'}>
+        <div className={'backToCatalog row bg-primary'} onClick={() => this.props.setView('catalog', {})}>Back to Catalog</div>
+        <div className={'row'}>
+          <div className={'col bg-success'}>
+            <img className={'productDetailImage'} src='./images/shamwow.jpg' style={{ height: '200px', width: '200px' }}></img>
+          </div>
+          <div className={'col bg-danger'}>
+            <h5 className={'productDetailsTitle'}>SHAMWOWZERS</h5>
+            <h6 className={'productDetailPrice'}>$1000</h6>
+            <p className={'productDetailShortDescription'}>
               It's like a chamois, towel, and sponge, all in one! Soaks up to 10x it's weight in any liquid!
           </p>
+          </div>
         </div>
-        <div className={'productDetailsLongDescription'}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, sapiente iusto corporis dolorem sit reiciendis deserunt? Qui veritatis maxime consequatur!
+        <div className={'row bg-warning'}>
+          <div className={'productDetailsLongDescription'}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, sapiente iusto corporis dolorem sit reiciendis deserunt? Qui veritatis maxime consequatur!
+        </div>
         </div>
       </div>
+
+
+
+      // <div className={'container w-75 p-3 bg-secondary'}>
+      //   <div className={'backToCatalog'} onClick={() => this.props.setView('catalog', {})}>Back to Catalog</div>
+        // <img className={'productDetailImage'} src='./images/shamwow.jpg' style={{ height: '200px', width: '200px' }}></img>
+      //   <div className={'productDetailsContainer bg-danger'}>
+          // <h5 className={'productDetailsTitle'}>SHAMWOWZERS</h5>
+          // <h6 className={'productDetailPrice'}>$1000</h6>
+          // <p className={'productDetailShortDescription'}>
+          //     It's like a chamois, towel, and sponge, all in one! Soaks up to 10x it's weight in any liquid!
+          // </p>
+      //   </div>
+        // <div className={'productDetailsLongDescription'}>
+        //   Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, sapiente iusto corporis dolorem sit reiciendis deserunt? Qui veritatis maxime consequatur!
+        // </div>
+      // </div>
     );
   }
 }
